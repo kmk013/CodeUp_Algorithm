c64ab282dd44cc5c491c6841f91c25132138196d
k3h1i013 <kmk013@naver.com>
2017-10-02 00:35:37 +0900

    CodeUp_First_Commit

---
+++ 1002_출력하기02/1002_출력하기02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1002_출력하기02
+//
+//  Created by 김민규 on 2017. 10. 2..
+//  Copyright © 2017년 김민규. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

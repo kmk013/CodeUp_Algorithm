72080937099cf7343bb95af2701b9dd5dcdbea7a
k3h1i013 <kmk013@naver.com>
2017-10-02 10:31:21 +0900

    CodeUp_1019

---
+++ 1019_년월일 입력받아 형식에 맞게 출력하기/1019_년월일 입력받아 형식에 맞게 출력하기/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int year, month, day;
+    
+    scanf("%d.%d.%d", &year, &month, &day);
+    printf("%04d.%02d.%02d", year, month, day);
+    
+    return 0;
+}

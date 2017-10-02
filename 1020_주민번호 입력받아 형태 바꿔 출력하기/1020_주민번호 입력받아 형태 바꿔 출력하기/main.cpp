e423ac1191725e2e49516ac41bc1826e0c27123d
k3h1i013 <kmk013@naver.com>
2017-10-02 10:40:14 +0900

    CodeUp_1020

---
+++ 1020_주민번호 입력받아 형태 바꿔 출력하기/1020_주민번호 입력받아 형태 바꿔 출력하기/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int front, back;
+    
+    scanf("%d-%d", &front, &back);
+    printf("%06d%07d", front, back);
+    
+    return 0;
+}

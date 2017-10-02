d3d6fc479de6a4c19b2e86d8429333a734401282
k3h1i013 <kmk013@naver.com>
2017-10-02 10:27:47 +0900

    CodeUp_1018

---
+++ 1018_시간 입력받아 그대로 출력하기/1018_시간 입력받아 그대로 출력하기/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int hour, minute;
+    
+    scanf("%d:%d", &hour, &minute);
+    cout << hour << ":" << minute;
+    
+    return 0;
+}

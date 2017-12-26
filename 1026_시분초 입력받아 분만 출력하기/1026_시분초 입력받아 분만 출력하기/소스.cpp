85d7330e45757feedaffa832c65e93b93ebb63d4
k3h1i013 <kmk013@naver.com>
2017-12-26 09:54:47 +0900

    CodeUp_1026

---
+++ 1026_시분초 입력받아 분만 출력하기/1026_시분초 입력받아 분만 출력하기/소스.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	int hour, minute, second;
+
+	scanf("%d:%d:%d", &hour, &minute, &second);
+	cout << minute << endl;
+
+	return 0;
+}
